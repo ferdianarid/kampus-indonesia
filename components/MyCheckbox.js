2437d4274a360946d3de52ee6950e9aa0acc7583
dev-dikirim-in <dev.dikirim.in@gmail.com>
2021-09-22 20:59:12 +0700

    add my component

---
+++ components/MyCheckbox.js
@@ -0,0 +1,17 @@
+const MyCheckbox = ({ label }) => {
+    return (
+        <div className="flex items-center">
+            <label className="flex justify-start items-start">
+                <div className="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-primary">
+                    <input value="" type="checkbox" className="opacity-0 absolute" />
+                    <svg className="fill-current hidden w-4 h-4 text-primary pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z" /></svg>
+                </div>
+            </label>
+            <label htmlFor="selectAll" className="text-xs ml-2">
+                {label}
+            </label>
+        </div>
+    )
+}
+
+export default MyCheckbox
