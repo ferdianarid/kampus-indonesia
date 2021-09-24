f3e2a02b92a5e17445fc5a8d49b721862c213ed4
dev-dikirim-in <dev.dikirim.in@gmail.com>
2021-09-24 23:31:13 +0700

    create my component

---
+++ components/inputs/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = React.forwardRef(({ children, className, ...props }, ref) => {
+    return (
+        <button
+            ref={ref}
+            className={`${className} rounded-md py-2 px-3 bg-primary text-white`}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+});
+
+export default Button;
