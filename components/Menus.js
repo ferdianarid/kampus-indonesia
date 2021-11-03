6e5426a21856c8531956c14a6b36ba12576668d2
rizki36 <cahrizki.rf@gmail.com>
2021-11-04 00:48:12 +0700

    done

--- components/Menus.js
+++ components/Menus.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Menus = () => {
     return (
@@ -13,34 +13,53 @@ const Menus = () => {
                 <Menu href="/informations/internship">Magang</Menu>
             </ContainerSubmenu>
         </div>
-    )
-}
+    );
+};
 
 const ContainerSubmenu = ({ text, children }) => {
-    const [isOpen, setIsOpen] = React.useState(false)
+    const { pathname } = useRouter();
+
+    const containerUrl = pathname.split("/").at(1); // ['', 'informations', 'event']
+    const itsOpened = containerUrl === "informations";
+    const [isOpen, setIsOpen] = React.useState(itsOpened);
+    
+    // active when submenu opened
     return (
         <div className="bg-[#356395] bg-opacity-30">
-            <div onClick={() => setIsOpen(!isOpen)} className={`px-5 cursor-pointer py-3 text-white hover:bg-[#356395] flex justify-between items-center  ${isOpen && 'bg-[#356395]'}`}>
+            <div
+                onClick={() => setIsOpen(!isOpen)}
+                className={`px-5 cursor-pointer py-3 text-white hover:bg-[#356395] flex justify-between items-center  ${
+                    isOpen && "bg-[#356395]"
+                }`}
+            >
                 {text}
-                <img src="/icons/chevron-right.svg" width="5px" className={`${isOpen && 'rotate-90'}`} />
+                <img
+                    src="/icons/chevron-right.svg"
+                    width="5px"
+                    className={`${isOpen && "rotate-90"}`}
+                />
             </div>
             {isOpen && children}
         </div>
-    )
-}
+    );
+};
 
 const Menu = ({ href, children }) => {
-    const { pathname } = useRouter()
+    const { pathname } = useRouter();
 
     // active when url is this page
-    const itsPage = pathname === href
+    const itsPage = pathname === href;
     return (
         <Link href={href}>
-            <a className={`${itsPage && 'bg-[#356395]'} cursor-pointer px-5 py-3 text-white hover:bg-[#356395] flex justify-between items-center`}>
+            <a
+                className={`${
+                    itsPage && "bg-[#356395]"
+                } cursor-pointer px-5 py-3 text-white hover:bg-[#356395] flex justify-between items-center`}
+            >
                 {children}
             </a>
         </Link>
-    )
-}
+    );
+};
 
-export default Menus
+export default Menus;
