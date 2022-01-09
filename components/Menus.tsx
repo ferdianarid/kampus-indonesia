aeef9b3cf0c29e7359454d831f13caa94c135922
Rizki36 <cahrizki.rf@gmail.com>
2022-01-09 23:32:20 +0700

    refactor : create, update & delete done

--- components/Menus.tsx
+++ components/Menus.tsx
@@ -15,7 +15,7 @@ const Menus = () => {
       </ContainerSubmenu>
       <ContainerSubmenu link="informations" text="Informasi">
         <Menu href="/informations/university">Kampus / Jurusan</Menu>
-        <Menu href="/informations/scholarship">Beasiswa</Menu>
+        <Menu href="/informations/schoolarship">Beasiswa</Menu>
         <Menu href="/informations/event">Event</Menu>
         <Menu href="/informations/internship">Magang</Menu>
       </ContainerSubmenu>
---
+++ components/domain/informations/schoolarship/Form.tsx
@@ -0,0 +1,221 @@
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Input from "@components/inputs/Input";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import Button from "@components/inputs/Button";
+import File from "@components/inputs/File";
+import Editor from "@components/inputs/TextEditor";
+import backendApi from "configs/api/backendApi";
+import { IForm } from "interfaces/Schoolarship";
+import { AxiosError, AxiosRequestConfig } from "axios";
+import { toast } from "react-toastify";
+import { useSession } from "next-auth/react";
+import { ErrApi } from "interfaces";
+import router from "next/router";
+import { getFirstErrorFromArray } from "@utils/index.js";
+import { useSchoolarshipForm } from "hooks/swr/schoolarship";
+import Spinner from "@components/Spinner";
+
+const Form = ({ id }: { id?: number }) => {
+  const { data: session } = useSession();
+  const reqConfig: AxiosRequestConfig = {
+    headers: {
+      Authorization: "Bearer " + session.access_token,
+    },
+  };
+
+  const { data, isError, isLoading, mutate } = useSchoolarshipForm(
+    reqConfig,
+    id
+  );
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    setValue,
+  } = useForm<IForm>({
+    mode: "onSubmit",
+    resolver: yupResolver(
+      yup.object({
+        title: yup.string().required().label("Judul Beasiswa"),
+        description: yup.string().required().label("Deskripsi"),
+        level: yup.string().required().label("Level"),
+        category: yup.string().required().label("Kategori"),
+        accreditation: yup.string().required().label("Akreditasi"),
+        type: yup.string().required().label("Tipe"),
+        deadline: yup.string().required().label("Deadline"),
+        registration_link: yup.string().required().label("Link Registrasi"),
+      })
+    ),
+  });
+
+  const submit: SubmitHandler<IForm> = async ({
+    title,
+    description,
+    level,
+    category,
+    covers,
+    accreditation,
+    type,
+    deadline,
+    registration_link,
+  }) => {
+    let endPoint = "/panel/schoolarships/store";
+
+    if (id) {
+      endPoint = `/panel/schoolarships/update/${id}`;
+    } else {
+      if (!covers.length) {
+        return toast("Cover diperlukan !", {
+          type: "warning",
+        });
+      }
+    }
+
+    const myToast = toast.loading("Tunggu sebentar...");
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("level", level);
+    formData.append("category", category);
+    formData.append("accreditation", accreditation);
+    formData.append("type", type);
+    formData.append("deadline", deadline);
+    formData.append("registration_link", registration_link);
+    formData.append("publish", "");
+    if (covers.length) formData.append("logo", covers[0]);
+
+    backendApi
+      .post(endPoint, formData, reqConfig)
+      .then((res) => {
+        toast.update(myToast, {
+          render: "Yeyyy aksi berhasil",
+          type: "success",
+          isLoading: false,
+          autoClose: 1500,
+          closeButton: true,
+        });
+
+        mutate();
+
+        setTimeout(() => {
+          router.push("/informations/schoolarship");
+        }, 2000);
+      })
+      .catch(({ response: { data }, code }: AxiosError<ErrApi>) => {
+        if (data.errors) {
+          toast.update(myToast, {
+            render: getFirstErrorFromArray(data.errors),
+            type: "error",
+            isLoading: false,
+            autoClose: 2000,
+            closeButton: true,
+          });
+        } else {
+          toast.update(myToast, {
+            render: data.message ?? "Upsss aksi gagal",
+            type: "error",
+            isLoading: false,
+            autoClose: 2000,
+            closeButton: true,
+          });
+        }
+      });
+  };
+  if (id && isLoading) return <Spinner />;
+
+  return (
+    <form
+      onSubmit={handleSubmit(submit)}
+      action=""
+      className="grid lg:grid-cols-2 gap-y-5 gap-x-7"
+    >
+      <Input
+        label="Judul Beasiswa"
+        defaultValue={data?.title}
+        {...register("title")}
+        {...(errors.title?.message && {
+          errorMessage: errors.title?.message,
+        })}
+      />
+      <Input
+        label="Level"
+        defaultValue={data?.level}
+        {...register("level")}
+        {...(errors.level?.message && {
+          errorMessage: errors.level?.message,
+        })}
+      />
+      <Input
+        label="Kategori"
+        defaultValue={data?.category}
+        {...register("category")}
+        {...(errors.category?.message && {
+          errorMessage: errors.category?.message,
+        })}
+      />
+      <Input
+        label="Akreditasi"
+        defaultValue={data?.accreditation}
+        {...register("accreditation")}
+        {...(errors.accreditation?.message && {
+          errorMessage: errors.accreditation?.message,
+        })}
+      />
+      <Input
+        label="Tipe"
+        defaultValue={data?.type}
+        {...register("type")}
+        {...(errors.type?.message && {
+          errorMessage: errors.type?.message,
+        })}
+      />
+      <Input
+        label="Deadline"
+        defaultValue={data?.deadline}
+        type={"date"}
+        {...register("deadline")}
+        {...(errors.deadline?.message && {
+          errorMessage: errors.deadline?.message,
+        })}
+      />
+      <Input
+        label="Link Pendaftaran"
+        defaultValue={data?.registration_link}
+        {...register("registration_link")}
+        {...(errors.registration_link?.message && {
+          errorMessage: errors.registration_link?.message,
+        })}
+      />
+
+      <File label="Upload Cover" {...register("covers")} />
+
+      <div className="col-span-1 lg:col-span-2">
+        <Controller
+          name="description"
+          control={control}
+          render={(field) => {
+            return (
+              <Editor
+                onInit={() => {
+                  setValue("description", data?.description);
+                }}
+                defaultValue={data?.description}
+                {...field.field}
+              />
+            );
+          }}
+        />
+      </div>
+
+      <div>
+        <Button type="submit">Simpan</Button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
